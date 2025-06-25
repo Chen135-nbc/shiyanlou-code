8485c094d6323fbf9f0fafbb93261465735b52f2
Chen135-nbc <3042652047@qq.com>
2025-06-25 20:41:41 +0800

    Delete StringBuilderPractice directory

--- StringBuilderPractice/demo4.java
+++
@@ -1,8 +0,0 @@
-public class demo4 { //替换子字符串
-    public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("Hello Java World");
-        StringBuilder childStr = new StringBuilder("Python");
-        str.replace(6,10,childStr.toString());
-        System.out.println(str);
-    }
-}
